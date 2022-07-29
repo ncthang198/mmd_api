6beab6c427e3490572b6b2fdf768d1f60fd44e12
thangnc <thangnc11@viettelpost.com.vn>
2022-07-29 17:47:34 +0700

    ABC

--- routes/mmd.js
+++ routes/mmd.js
@@ -1,8 +1,10 @@
 var express = require('express');
 const { getFirestore } = require('firebase-admin/firestore');
 const fs = require('firebase-admin');
+const xlsx = require('node-xlsx');
 
-const serviceAccount = require('../config/mmd_service_account_firebase.json');
+
+const serviceAccount = require('../config/service-account-firebase.json');
 fs.initializeApp({
   credential: fs.credential.cert(serviceAccount)
 });
@@ -45,6 +47,23 @@ router.get('/getRandomQuote', async function (req, res, next) {
   })
 });
 
+router.get('/getRandomByCategory', async function (req, res, next) {
+  // Create a reference to the cities collection
+  const { category } = req.body;
+  const quoteRef = db.collection('quotes');
+  const queryRef = quoteRef.where('category', '==', category);
+  const quotesCategory = await queryRef.get();
+  const quotes = []
+  quotesCategory.forEach((doc)=>{
+    quotes.push(doc.data());
+  })
+  res.send({
+    isSuccess: true,
+    quotes: quotes
+  })
+});
+
+
 router.post('/addQuotes', async function (req, res, next) {
   try {
     const quotes = req.body;
@@ -64,6 +83,31 @@ router.post('/addQuotes', async function (req, res, next) {
     }
   } catch (error) {
     res.send({ isSuccess: false })
+  }
+});
+router.post('/addQuotesFromExcel', async function (req, res, next) {
+  try {
+    var obj = xlsx.parse(__dirname + '/data.xlsx');
+
+    var batch = db.batch();
+
+    for (let i = 1; i < obj[0].data.length - 1; i++) {
+      const e = obj[0].data[i];
+      console.log(e[1], e[2]);
+      let doc = {
+        quote: e[1],
+        category: e[2]
+      }
+      var docRef = db.collection("quotes").doc(); //automatically generate unique id
+      batch.set(docRef, doc);
+    }
+    const resFirebase = await batch.commit();
+    if (resFirebase) {
+      res.send({ isSuccess: true })
+    }
+  } catch (error) {
+    console.log(error);
+    res.send({ isSuccess: false, error })
   }
 });
 
